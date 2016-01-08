1c2f03a552d7484d58dab206713201a2182d0fda
reinka <andrei.poehlmann90@gmail.com>
2016-01-08 11:14:20 +0100

    Blatt 10, Aufgabe 5

---
+++ ComputerScience/prog/blatt10/a5.c/a5.c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  a5.c
+//
+//  Created by Andrei Poehlmann on 07/01/16.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
