db0ac54998011ddff38afefa38964bd03e8361b2
reinka <andrei.poehlmann90@gmail.com>
2016-02-06 14:30:06 +0100

    various C exercises / projects

---
+++ C/workspace/linked_list/src/linked_list.h
@@ -0,0 +1,17 @@
+/*
+ * linked_list.h
+ *
+ *  Created on: Feb 6, 2016
+ *      Author: slackoverflow
+ */
+
+#ifndef LINKED_LIST_H_
+#define LINKED_LIST_H_
+
+/*list element*/
+struct ielem{
+	int val;
+	struct ielem *next;
+};
+
+#endif /* LINKED_LIST_H_ */
