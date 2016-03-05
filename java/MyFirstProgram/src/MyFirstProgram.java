a79571934d240a7cc8ad93e0ae53a93d93176063
reinka <andrei.poehlmann90@gmail.com>
2016-03-05 02:17:13 +0100

    First program

---
+++ java/MyFirstProgram/src/MyFirstProgram.java
@@ -0,0 +1,11 @@
+public class MyFirstProgram {
+	public static void main(String[] args){
+		System.out.println("Hello world.");
+		
+		int x, y, z;
+		x = 8;
+		y = 6;
+		z = x*y;
+		System.out.println("Result: x*y = " + z + "where x: " + x + "and y: "+y);
+	}
+}
